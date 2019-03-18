bcda2f1907f89b4f6dd18c01638980cd0e82b773
evilthed4nte <evilthedante@gmail.com>
2019-03-18 09:56:48 +0800

    pagnation
    pagnation

--- blog/views.py
+++ blog/views.py
@@ -9,93 +9,10 @@ class IndexView(ListView):
     context_object_name = 'article_list'
 
     # 以10为分页 某种不知名的力量...
-    paginate_by = 10
-
-    def get_context_data(self, **kwargs):
-        # 获得父类生成的传递给模板的字典 以及三个参数
-        context = super().get_context_data(**kwargs)
-        paginator = context.get('paginator')
-        page = context.get('page_obj')
-        is_paginated = context.get('is_paginated')
-
-        # 调用 pagination_data 方法获得显示分页导航条需要的数据，见下方。
-        pagination_data = self.pagination_data(paginator, page, is_paginated)
-        # 将分页导航条的模板变量更新到 context 中，注意 pagination_data 方法返回的也是一个字典。
-        context.update(pagination_data)
-        # context 字典中已有了显示分页导航条所需的数据。
-        return context
-
-    def pagination_data(self, paginator, page, is_paginated):
-        if not is_paginated:
-            # 如果没有分页，则无需显示分页导航条，不用任何分页导航条的数据
-            return {}
-
-        left = []
-        right = []
-
-        # 标示第 1 页页码后是否需要显示省略号
-        left_has_more = False
-
-        # 标示最后一页页码前是否需要显示省略号
-        right_has_more = False
-
-        first = False
-
-        last = False
-
-        # 获得用户当前请求的页码号
-        page_number = page.number
-
-        # 获得分页后的总页数
-        total_pages = paginator.num_pages
-
-        # 获得整个分页页码列表，比如分了四页
-        page_range = paginator.page_range
-
-        if page_number == 1:
-            right = page_range[page_number:page_number + 2]
-
-            if right[-1] < total_pages - 1:
-                right_has_more = True
-
-            if right[-1] < total_pages:
-                last = True
-
-        elif page_number == total_pages:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-
-            if left[0] > 2:
-                left_has_more = True
-
-            if left[0] > 1:
-                first = True
-        else:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-            right = page_range[page_number:page_number + 2]
-
-            if right[-1] < total_pages - 1:
-                right_has_more = True
-            if right[-1] < total_pages:
-                last = True
-
-            if left[0] > 2:
-                left_has_more = True
-            if left[0] > 1:
-                first = True
-
-        data = {
-            'left': left,
-            'right': right,
-            'left_has_more': left_has_more,
-            'right_has_more': right_has_more,
-            'first': first,
-            'last': last,
-        }
-
-        return data
+    paginate_by = 7
 
 
-# 分类详情页
+# 分类详情页 继承IndexView
 class CategoryView(IndexView):
     # model = Category
     # template_name = 'blog/index.html'
