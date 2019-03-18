c6b4fa6a306344dfde3a1eeb95fe408261c68ca0
evilthed4nte <evilthedante@gmail.com>
2019-03-18 09:30:51 +0800

    pagnation
    add pagnation
    delete shuven

--- blog/urls.py
+++ blog/urls.py
@@ -6,14 +6,9 @@ app_name = 'blog'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.IndexView.as_view(), name='index'),
-    path('loving-shuven', views.shuven, name='shuven'),
+    # path('loving-shuven', views.shuven, name='shuven'),
     path('article/<int:pk>/', views.article, name='reverse_article_url'),
     path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
     path('archives/<int:year>/<int:month>/', views.ArchivesView.as_view(), name='archives'),
     path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
-    # url(r'^$', views.index, name='show_index'),
-    # url(r'^article/(?P<pk>[0-9]+)/$', views.article, name='detail'),
-    # url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
-    # url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
-    # url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,25 +2,97 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from blog.models import Article, Category, Tag
 
-
-def shuven(request):
-    shuven = Article.objects.get(pk=11)
-    import markdown
-    shuven.body = markdown.markdown(shuven.body, extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
-    ])
-    return render(request, 'blog/index-s.html', context={
-        'shuven': shuven
-    })
-
-
 # 类视图
 class IndexView(ListView):
     model = Article
     template_name = 'blog/index.html'
     context_object_name = 'article_list'
+
+    # 以10为分页 某种不知名的力量...
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # 获得父类生成的传递给模板的字典 以及三个参数
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        # 调用 pagination_data 方法获得显示分页导航条需要的数据，见下方。
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        # 将分页导航条的模板变量更新到 context 中，注意 pagination_data 方法返回的也是一个字典。
+        context.update(pagination_data)
+        # context 字典中已有了显示分页导航条所需的数据。
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            # 如果没有分页，则无需显示分页导航条，不用任何分页导航条的数据
+            return {}
+
+        left = []
+        right = []
+
+        # 标示第 1 页页码后是否需要显示省略号
+        left_has_more = False
+
+        # 标示最后一页页码前是否需要显示省略号
+        right_has_more = False
+
+        first = False
+
+        last = False
+
+        # 获得用户当前请求的页码号
+        page_number = page.number
+
+        # 获得分页后的总页数
+        total_pages = paginator.num_pages
+
+        # 获得整个分页页码列表，比如分了四页
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
 
 
 # 分类详情页
@@ -47,15 +119,7 @@ class ArchivesView(ListView):
         return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
                                                                created_time__month=month)
 
-    # def get_queryset(self):
-    #     year = self.kwargs.get('year')
-    #     month = self.kwargs.get('month')
-    #     monthrange = calendar.monthrange(int(year), int(month))
-    #     article_list = super(ArchivesView, self).get_queryset().filter(
-    #         created_time__range=(
-    #             datetime.date(int(year), int(month), 1),
-    #             datetime.date(int(year), int(month), monthrange[1])))
-    #     return article_list
+
 
 
 # 标签页
@@ -63,6 +127,7 @@ class TagView(ListView):
     model = Article
     template_name = 'blog/index.html'
     context_object_name = 'article_list'
+
 
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
@@ -81,6 +146,10 @@ def article(request, pk):
     return render(request, 'blog/detail.html', context={
         'article_detail': article_detail
     })
+
+
+
+
 
 
 # TODO
